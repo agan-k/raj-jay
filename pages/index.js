d0bf64ab073ccc59937bb1e125469c3d312d6fa1
Koran Agan <koranagan@gmail.com>
2021-03-18 13:13:05 -0400

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,18 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Prismic from "prismic-javascript"
+import { client } from "../prismic-configuration"
 
-export default function Home() {
+export default function Home(props) {
+   console.log(props.news_cards.results)
+   const cards = props.news_cards.results.map(item => 
+      <a href="#" className={styles.card}>
+         <img src={item.data.news_card_image.url} />
+            <h3>{item.data.title[0].text} &rarr;</h3>
+         <p>{item.data.news_card_date[0].text}</p>
+         <p>{item.data.news_card_blurb[0].text}</p>
+          </a>
+      )
   return (
     <div className={styles.container}>
       <Head>
@@ -11,55 +22,41 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Rajiv jayaweera
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Home of drummer Rajiv jayaweera
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+         <div className={styles.grid}>
+            {cards}
+          
+         </div>
       </main>
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://formversuscontent.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          Site by{' form vs content'}
+          {/* <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} /> */}
         </a>
       </footer>
     </div>
   )
+}
+
+export async function getStaticProps() {
+   const news_cards = await client.query(
+      Prismic.Predicates.at("document.type", "news_card"),
+      { orderings: "[my.news_card.date desc]" }
+   )
+   return {
+      props: {
+         news_cards
+      },
+   }
 }
