652e4e6b20023cf7def1ebbd601f7a94c89ea617
Koran Agan <koranagan@gmail.com>
2021-03-27 11:13:13 -0400

    render all press sub types and style the list

--- pages/press/index.js
+++ pages/press/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-// import styles from '../styles/Home.module.css'
+import styles from '../../styles/reviews.module.css'
 import Prismic from "prismic-javascript"
 import { client } from "../../prismic-configuration"
 import { React } from 'react'
@@ -8,13 +8,22 @@ import Link from "next/link";
 
 export default function reviews(props) {
    console.log(props.content)
-   // console.log(props.press.results[0].data.press_body[0].text)
-   // console.log(props.press.results.map(result => 
-   //    result.data.press_body[0].text))
-   // console.log(props.press.results[0].data.article_title[0].text)
+
+   const press_reviews = props.content.results.filter(result => 
+      result.data.content_type == 'press/reviews'
+   )
+   const press_releases = props.content.results.filter(result => 
+      result.data.content_type == 'press/releases'
+   )
+   const press_interviews = props.content.results.filter(result => 
+      result.data.content_type == 'press/interviews'
+   )
+   const press_features = props.content.results.filter(result => 
+      result.data.content_type == 'press/features'
+   )
    
    return (
-      <div>
+      <div className={styles.container}>
          <h1>&larr;
             <Link href="/">
                <a>
@@ -23,22 +32,68 @@ export default function reviews(props) {
             </Link>
          </h1>
          <h1>reviews</h1>
+          <ul>
+             {press_reviews.map((result) => (
+                 <li key={result.uid} style={{ background: 'grey' }}>
+                    <Link href="press/reviews/[id]" as={`press/reviews/${result.uid}`}>
+                       <a>
+                          {RichText.render(result.data.content_body.filter(item => item.type == 'heading3'))}
+                          &nbsp; - &nbsp;
+                          {RichText.render(result.data.content_body.filter(item => item.type == 'heading4'))}
+                       </a>
+                     </Link>
+                  </li>
+               ))}
+         </ul>
+         <hr />
+         
+         <h1>interviews</h1>
          <ul>
-        {props.content.results.map((result) => (
-           <li key={result.uid}>
-              <Link href="press/reviews/[id]" as={`press/reviews/${result.uid}`}>
-                 <a>
-                    {/* {RichText.render(result.data.publication)} */}
-                    {RichText.render(result.data.content_body.filter(item => item.type == 'heading3'))}
-                    {RichText.render(result.data.content_body.filter(item => item.type == 'heading4'))}
-                    {/* {RichText.render(result.data.article_title)} */}
-                 </a>
-              </Link>
-          </li>
-        ))}
-      </ul>
+            {press_interviews.map((result) => (
+               <li key={result.uid} style={{ background: 'red' }}>
+                  <Link href="press/reviews/[id]" as={`press/reviews/${result.uid}`}>
+                     <a>
+                        {RichText.render(result.data.content_body.filter(item => item.type == 'heading3'))}
+                        &nbsp; - &nbsp;
+                        {RichText.render(result.data.content_body.filter(item => item.type == 'heading4'))}
+                     </a>
+                  </Link>
+               </li>
+            ))}
+         </ul>
+         <hr />
+         
+         <h1>press releases</h1>
+         <ul>
+            {press_releases.map((result) => (
+               <li key={result.uid} style={{ background: 'green' }}>
+                  <Link href="press/reviews/[id]" as={`press/reviews/${result.uid}`}>
+                     <a>
+                        {RichText.render(result.data.content_body.filter(item => item.type == 'heading3'))}
+                        &nbsp; - &nbsp;
+                        {RichText.render(result.data.content_body.filter(item => item.type == 'heading4'))}
+                     </a>
+                  </Link>
+               </li>
+            ))}
+         </ul>
         <hr/>
-        <h1>interviews</h1>
+         <h1>features</h1>
+         
+         <ul>
+            {press_features.map((result) => (
+               <li key={result.uid} style={{ background: 'yellow' }}>
+                  <Link href="press/reviews/[id]" as={`press/reviews/${result.uid}`}>
+                     <a>
+                        {RichText.render(result.data.content_body.filter(item => item.type == 'heading3'))}
+                        &nbsp; - &nbsp;
+                        {RichText.render(result.data.content_body.filter(item => item.type == 'heading4'))}
+                     </a>
+                  </Link>
+               </li>
+            ))}
+         </ul>
+        <hr/>
       </div>
    )
 }
