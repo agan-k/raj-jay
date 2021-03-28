a6cd5c7c3a18796b21e8f41c40e5724df8fcf6d1
Koran Agan <koranagan@gmail.com>
2021-03-27 22:56:21 -0400

    sset up photo gallery and adjust prismic content type for new photos

--- pages/photos/index.js
+++ pages/photos/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Prismic from 'prismic-javascript'
 import { client } from '../../prismic-configuration'
+import style from './photos.module.css'
 
 export default class Photos extends Component {
    constructor(props) {
@@ -11,12 +12,26 @@ export default class Photos extends Component {
       }
    }
    render() {
-      const photos = this.props.content.results.map(result =>
-         <img src={result.data.img.url}/>
-         )
+      const photos = this.props.content.results.filter(result => 
+         result.data.content_type == 'photo'
+      )
+      console.log(photos)
+      const gallery = photos.map(result => 
+         result.data.img.dimensions.height > result.data.img.dimensions.width ?
+         <div className={style.photo_container}>
+               <img className={style.vertical_img} src={result.data.img.url} />
+         </div> :
+         <div className={style.photo_container}>
+               <img className={style.horizontal_img} src={result.data.img.url} /> 
+         </div>
+      )
       return (
          <div>
             <h1>photos</h1>
+            <div className={style.gallery_container}>
+               {gallery}
+
+            </div>
          </div>
       )
    }
