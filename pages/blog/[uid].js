1d42ff18416d25d1e7b0b223bd9bc9deb9a49509
Koran Agan <koranagan@gmail.com>
2021-04-14 19:35:57 -0400

    remove blog and disco/index links

--- pages/blog/[uid].js
+++ pages/blog/[uid].js
@@ -16,7 +16,7 @@ export default function Post({ data }) {
    return (
       <Layout>
          <div className={style.container}>
-         <Link href="/blog"><h4>&larr; &nbsp;<a>blog home</a></h4></Link>
+         {/* <Link href="/blog"><h4>&larr; &nbsp;<a>blog home</a></h4></Link> */}
             {data.video_link.length !== 0 ?
                <article>
                   <h3>{formatPrismicDate(data.date)}</h3>
