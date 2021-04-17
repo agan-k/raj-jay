856e0cbc16b2cc0c8b8c4294e5cefe554f34f205
Koran Agan <koranagan@gmail.com>
2021-04-17 00:21:25 -0400

    configure contact form

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -5,36 +5,18 @@ import { RichText } from 'prismic-reactjs'
 
 import style from './contact.module.css'
 import Layout from '../../components/layout'
+import ContactForm from '../../components/contactForm'
 
 export default function Contact(props) {
    
    return (
       <Layout>
-         <div className={style.container}>
+         {/* <div className={style.container}>
             <div className={style.contact_container}>
                {RichText.render(props.contact.data.contact_body)}
             </div>
-            <form name="contact" method="POST" data-netlify="true">
-               <p>
-                  <label>Your Name: <input type="text" name="name" /></label>   
-               </p>
-               <p>
-                  <label>Your Email: <input type="email" name="email" /></label>
-               </p>
-               <p>
-                  <label>Your Role: <select name="role[]" multiple>
-                     <option value="leader">Leader</option>
-                     <option value="follower">Follower</option>
-                  </select></label>
-               </p>
-               <p>
-                  <label>Message: <textarea name="message"></textarea></label>
-               </p>
-               <p>
-                  <button type="submit">Send</button>
-               </p>
-            </form>
-         </div>
+         </div> */}
+         <ContactForm/>
       </Layout>
    )
 }
---
+++ pages/formSubmission.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+export default function formSubmission() {
+   return (
+      <Layout>
+         <div style={{display: "flex", justifyItems: 'center', alignItems: "center", minHeight: "80vh", textAlign: "center", transition: "2s"}}>
+            <p style={{display: "block", width: "100%", textAlign: "center"}}>Successfully submitted!..</p>
+         </div>
+      </Layout>
+   )
+}
