3314be5ed2977573d3b6fd127a4d529def47514c
agan-k <koranagan@gmail.com>
2024-02-25 11:36:13 -0500

    bring in mediaQuery for Bio

--- pages/bio/styled.js
+++ pages/bio/styled.js
@@ -2,6 +2,12 @@ import styled from "styled-components";
 import { mediaQuery } from "../../utils/mediaQuery";
 
 export const Container = styled.div`
+h2 {
+  ${mediaQuery({
+    margin: ['0', 'unset']
+  })}
+  
+}
   h2, p {
     margin-bottom: ${({theme}) => theme.space[3]}px;
   }
@@ -10,10 +16,20 @@ export const Container = styled.div`
   }
   img {
     float: left;
-    margin-right: ${({theme}) => theme.space[4]}px;
-    margin-bottom: ${({theme}) => theme.space[4]}px;
     max-height: ${({theme}) => theme.space[8]}px;
     border: ${({theme}) => theme.borders.thinCharcoal};
     border-radius: 12px;
+    ${mediaQuery({
+      width: ['90%', 'unset'],
+      margin: [
+        ' 0 5%',
+        ({theme}) => `0 ${theme.space[4]}px ${theme.space[4]}px 0`,
+      ],
+      float: ['unset', 'left'],
+      maxHeight: [
+        'unset',
+        ({theme}) => `${theme.space[8]}px`,
+      ]
+    })}
   }
 `;
