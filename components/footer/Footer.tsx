5ba3c16bfdfa0996c541b63e1d53c684ed1cac7b
agan-k <koranagan@gmail.com>
2024-03-13 23:21:29 -0400

    adjust position

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,27 +1,22 @@
-import { Anchor, FlexBox, Text, Box } from "..";
+import { Anchor, FlexBox, Text, Box } from "../../components";
+import { Container, Flipper, FlipperWrapper } from './styled';
 
 export const Footer: React.FC = () => {
   return(
-    <FlexBox $justifyContent={'end'}>
-      <Box $marginBottom={14} $marginTop={14}>
-        <Anchor url={'https://formversuscontent.com'} target={'_blank'} $color={'grey'}>
-            <FlexBox $gap={'4px'}>
-              <Text 
-                $fontSize={12} 
-                $textTransform={'uppercase'} 
-                $fontWeight={100}>
-                  site by:
-              </Text>
-              <Text 
-                $fontFamily="monospace" 
-                $fontSize={12} 
-                $color="orange"
-              >
-                {'(form(vs(content)))'}
-              </Text>
-            </FlexBox> 
-        </Anchor>
-      </Box>
-    </FlexBox>
+    <Container>
+      <FlipperWrapper>
+        <Flipper>
+          <Anchor url={'https://formversuscontent.com'} target={'_blank'} $color={'orange'}>
+            <Text 
+              $fontFamily="courier new" 
+              $fontSize={14} 
+              $color="grey"
+            >
+              {'(form (vs (content)))'}
+            </Text>
+          </Anchor>
+        </Flipper>
+      </FlipperWrapper>
+    </Container>
   );
 }
