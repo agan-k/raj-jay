8fd933d1948c8b560d3847928b43ceafeb95c636
agan-k <koranagan@gmail.com>
2024-03-16 18:23:59 -0400

    remove background from calendar listing in mobile

--- components/calendar-listing/styled.ts
+++ components/calendar-listing/styled.ts
@@ -5,13 +5,17 @@ import { CalendarListingProps } from "./CalendarListing";
 export const Listing = styled.div<CalendarListingProps>`
   margin-bottom: ${({theme}) => theme.space[3]}px;
   padding: ${({theme}) => theme.space[3]}px;
-  background: ${({$nextShow, theme}) => !$nextShow ? theme.colors.diffused : ''};
+  // background: ${({$nextShow, theme}) => !$nextShow ? theme.colors.diffused : ''};
   ${mediaQuery({
     width: ['100%', ({$width}) => $width],
     marginBottom: [
       ({theme}) => theme.space[3]+'px',
       ({theme}) => theme.space[4]+'px',
     ],
+    background: [
+      'unset',
+      ({$nextShow, theme}) => !$nextShow ? theme.colors.diffused : ''
+    ]
   })}
 `;
 
